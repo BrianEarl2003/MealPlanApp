8fd8f19f364c298662a079f59d25f6171de3e877
lucas <mor18008@byui.edu>
2021-06-05 18:39:23 -0600

    Finished all the views

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,27 @@ router.get('/addRecipe', function(req, res, next) {
   res.render('addRecipe', { title: 'Add a Recipe' });
 });
 
+/* GET mealCalendar page */
+router.get('/mealCalendar', function(req, res, next) {
+  res.render('mealCalendar', { title: 'Meal Calendar' });
+});
+
+/* GET ingredientList page */
+router.get('/ingredientList', function(req, res, next) {
+  res.render('ingredientList', { title: 'List of Ingredients' });
+});
+
+/* GET planMeals page */
+router.get('/planMeals', function(req, res, next) {
+  res.render('planMeals', { title: 'Plan Your Meals' });
+});
+
 /* GET recipelist page. */
-router.get('/recipelist', function(req, res) {
+router.get('/recipeList', function(req, res) {
   var db = req.db;
   var collection = db.get('recipeList');
   collection.find({},{},function(e,docs){
-      res.render('recipelist', {
+      res.render('recipeList', {
           "recipelist" : docs
       });
   });
