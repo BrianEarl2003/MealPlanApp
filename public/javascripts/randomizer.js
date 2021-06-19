cf0e1610235622697f24d0025fc606504f33f3f9
Lucas Moreira <lucasbrandm@gmail.com>
2021-06-19 17:27:52 -0600

    fixed the randomizer function

--- public/javascripts/randomizer.js
+++ public/javascripts/randomizer.js
@@ -1,24 +1,27 @@
 function randomizeSelect(selectId) {
 
     var options = document.getElementById(selectId).children;
-
-    var random = Math.floor(options.length * (Math.random() % 1));
-
+    
+    var random = 0;
+    do {
+        random = Math.floor(options.length * (Math.random() % 1));
+    } while (random == 0);
+    
     var option = options[random];
     option.selected = true;
     
-}
-
-var values = {};
-
-window.onload = function(e) {
-
+    }
+    
+    var values = {};
+    
+    window.onload = function(e) {
+    
     document.onchange = function(e) { // Event Delegation: http://davidwalsh.name/event-delegate
         var t = e.target;
         if(t.tagName == "SELECT")
             document.getElementById(t.id.replace("Select")).value = t.children[t.selectedIndex].innerHTML;  
     }
-
+    
     document.oninput = function(e) {
         var t = e.target;
         if(t.tagName == "INPUT") {
@@ -37,7 +40,7 @@ window.onload = function(e) {
             }  
         }
     }
-
+    
     document.getElementById("randomize").onclick = function(e) {
         randomizeSelect("sunday");
         randomizeSelect("monday");
@@ -46,18 +49,21 @@ window.onload = function(e) {
         randomizeSelect("thursday");
         randomizeSelect("friday");
         randomizeSelect("saturday");
-
+    
         //gotta make sure that they're all unique
         var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-
-        for (i = 0; i < 7; i++) {
-            var e = document.getElementById(days[i]);
-            var currentDayRecipe = e.options[e.selectedIndex].text;
-            for (j = 0; j < 7; j++) {
-                var f = document.getElementById(days[j]);
-                var otherDayRecipe = f.options[f.selectedIndex].text;
-                if ((currentDayRecipe == otherDayRecipe) || (currentDayRecipe == "")) {
-                    randomizeSelect(days[j]);
+    
+        for (x = 0; x < 15; x++) {
+          for (i = 0; i < 7; i++) {
+              var e = document.getElementById(days[i]);
+              var currentDayRecipe = e.options[e.selectedIndex].text;
+              for (j = i + 1; j < 7; j++) {
+                  var f = document.getElementById(days[j]);
+                  var otherDayRecipe = f.options[f.selectedIndex].text;
+                  if (currentDayRecipe == otherDayRecipe) {
+                     console.log(days[i] + " and " + days[j] + " match.");
+                     randomizeSelect(days[j]);
+                    }
                 }
             }
         }
