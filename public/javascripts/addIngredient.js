9e954d76661cfb46ba7635818d82e8d67ed8f61a
Brian Earl <yodaking_2003@hotmail.com>
2021-07-02 17:08:10 -0600

    restyled the addRecipe page

--- public/javascripts/addIngredient.js
+++ public/javascripts/addIngredient.js
@@ -1,20 +1,21 @@
-var numbers = ["Select Quantity", "⅛", "¼", "½", "⅓", "⅔", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var numbers = ["Select Quantity", "⅛", "⅜", "⅝", "⅞", "¼", "¾", "⅓", "⅔", "½", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var units = ["Select the Unit", "Tbsp", "tsp", "Oz", "Fl. Oz", "Cup", "qt", "pt", "gal", "lb", "Can", "unit"];
 
 function addFields(divName) {
     var newDiv = document.createElement('div');
+    newDiv.className = "row";
     var selectHTML = "";
-    selectHTML='<select name="ingQuant">';
+    selectHTML='<div class="col"><select class="form-control recipeForm" name="ingQuant">';
     for(i = 0; i < numbers.length; i = i + 1) {
         selectHTML += "<option value='" + numbers[i] + "'>" + numbers[i] + "</option>";
     }
-    selectHTML += "</select>";
-    selectHTML += '<select name="ingUnit">';
+    selectHTML += "</select></div>";
+    selectHTML += '<div class="col"><select class="form-control recipeForm" name="ingUnit">';
     for(i = 0; i < units.length; i = i + 1) {
         selectHTML += "<option value='" + units[i] + "'>" + units[i] + "</option>";
     }
-    selectHTML += "</select>";
-    selectHTML += '<textarea name="ingName" class="input" style="min-height:40px; max-height:40px;  width:68.5%" id="inputIngredientName" placeholder="Type Ingredient" name="ingName"></textarea>'
+    selectHTML += "</select></div>";
+    selectHTML += '<div class="col"><input type="text" name="ingName" class="input form-control recipeForm" "inputIngredientName" placeholder="Ingredient Name"></div>'
     newDiv.innerHTML = selectHTML;
     document.getElementById(divName).appendChild(newDiv);
 }
